2596c71b4e870b973301f192b0783d6a664a73b4
Alin RADU <alin.radu.cristian@gmail.com>
2025-01-03 17:45:42 +0200

    add spring-boot-overview

---
+++ section-01-spring-boot-overview/spring-boot-overview/src/main/java/com/dev/springBootOverview/SpringBootOverviewApplication.java
@@ -0,0 +1,13 @@
+package com.dev.springBootOverview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootOverviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootOverviewApplication.class, args);
+	}
+
+}
