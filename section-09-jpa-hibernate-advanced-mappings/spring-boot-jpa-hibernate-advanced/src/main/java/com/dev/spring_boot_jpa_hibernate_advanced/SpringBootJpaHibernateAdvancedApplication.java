dd56a4023866d58efb5cf6868960c4f1a4f8ff09
Alin RADU <alin.radu.cristian@gmail.com>
2025-01-09 16:20:21 +0200

    sync folders

---
+++ section-09-jpa-hibernate-advanced-mappings/spring-boot-jpa-hibernate-advanced/src/main/java/com/dev/spring_boot_jpa_hibernate_advanced/SpringBootJpaHibernateAdvancedApplication.java
@@ -0,0 +1,13 @@
+package com.dev.spring_boot_jpa_hibernate_advanced;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJpaHibernateAdvancedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJpaHibernateAdvancedApplication.class, args);
+	}
+
+}
---
+++ section-09-jpa-hibernate-advanced-mappings/spring-boot-jpa-hibernate-advanced/src/test/java/com/dev/spring_boot_jpa_hibernate_advanced/SpringBootJpaHibernateAdvancedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.spring_boot_jpa_hibernate_advanced;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJpaHibernateAdvancedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
